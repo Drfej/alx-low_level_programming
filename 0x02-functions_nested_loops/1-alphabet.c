0fad1ef96df099ad37a7fbfd7112bd09c24bea53
Drfej <fejjy1998@gmail.com>
2022-03-15 08:41:48 -0700

    lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *main - a function that prints the alphabet, in lowercase, followed by a new line
+ *
+ *Return: 0
+ */
+void print_alphabet(void)
+{
+char ch;
+ch = 'a';
+while (ch <= 'z')
+{
+_putchar(ch);
+ch++;
+}
+_putchar('\n');
+}
+
