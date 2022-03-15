af213c5c7824577378c0d0be399bf7b67afe4d4f
Drfej <fejjy1998@gmail.com>
2022-03-15 14:13:26 -0700

    001 commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 
 /**
  *main - Write a program that prints _putchar, followed by a new line
- *Return: Always 0 (Success)
+ *Return: Always 0 (success)
  */
+
 int main(void)
 {
 _putchar('_');
