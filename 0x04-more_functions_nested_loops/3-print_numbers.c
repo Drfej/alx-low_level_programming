d8048d4421544746c81ee784c3260994392ff906
Drfej <fejjy1998@gmail.com>
2022-03-17 11:38:50 -0700

    my commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,10 +3,11 @@
 /**
  *print_numbers - prints the numbers, from 0 to 9,
  *followed by a new line
+ *Return: 0
  */
 void print_numbers(void)
 {
-int 1;
+int i;
 for (i = 0; i < 10; i++)
 {
 _putchar(i + '0');
