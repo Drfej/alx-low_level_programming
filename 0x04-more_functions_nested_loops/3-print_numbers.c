725811771125cc403506612f8c391419aa02457b
Drfej <fejjy1998@gmail.com>
2022-03-17 13:26:09 -0700

    printing numbers commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- *print_numbers - prints the numbers,
- *from 0 to 9,
+ *print_numbers - prints the numbers, from 0 to 9,
  *followed by a new line
  */
 void print_numbers(void)
@@ -10,7 +9,7 @@ void print_numbers(void)
 int i;
 for (i = 0; i < 10; i++)
 {
-_putchar(i + 0);
+_putchar(i + '0');
 }
 _putchar('\n');
 }
