5fef83731c94ce374a6086e98df8d8dd96a5ee44
Drfej <fejjy1998@gmail.com>
2022-03-21 19:52:11 -0700

    destination

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,24 +2,18 @@
 
 /**
  *_strcpy - copies the string pointed to by src
- *including the terminating null byte (\0)
- *to the buffer pointed to by dest
+ *@dest: pointer to the buffer in which we copy the string
  *@src: string to be copied
- *
  *Return: the pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
-int len, i;
-len = 0;
-while (src[len] != '\0')
-{
-len++;
-
-}
-for (i = 0; i < len; i++)
+int i;
+i = 0;
+while (src[i] != '\0')
 {
 dest[i] = src[i];
+i++;
 }
 dest[i] = '\0';
 return (dest);
